37bd5898eb773fd7b01d89092f9533f23070f463
Andrew Kelley <andrew@ziglang.org>
2023-01-15 15:45:54 -0700

    build.zig: add target

--- build.zig
+++ build.zig
@@ -1,15 +1,16 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+    const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
     const lib = b.addStaticLibrary("xml", "Xml.zig");
+    lib.setTarget(target);
     lib.setBuildMode(mode);
     lib.install();
 
     const main_tests = b.addTest("Xml.zig");
+    main_tests.setTarget(target);
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
